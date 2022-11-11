51ed250e51f063bde8a7d4a28436cda90c8da19e
Shithappe <greenmad.b50@gmail.com>
2022-11-11 19:36:18 +0200

    again router

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
+// import Vue from 'vue'
 import { createRouter, createWebHistory  } from 'vue-router'
 import MainPage from './components/MainPage.vue';
 import FunVersion from './components/FunVersion.vue';
 
+// Vue.use(VueRouter)
+
 const routes = [
   { path: '/', component: MainPage },
-  { path: '/fun', component: FunVersion }
+  { path: '/fun', component: FunVersion, name: FunVersion }
 ]
 
 const router = createRouter({
